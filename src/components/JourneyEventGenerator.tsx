f2d9f29cc33e5775db5d531482d007180b124835
Adam Doan <adam.c.doan@gmail.com>
2025-07-27 14:12:02 -0400

    Now with event data for Hunter and Lookout

--- src/components/JourneyEventGenerator.tsx
+++ src/components/JourneyEventGenerator.tsx
@@ -4,22 +4,71 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import Button from '@mui/material/Button';
-import JourneyEvent from './JourneyEvent';
+import {Button, InputLabel, Select, MenuItem} from '@mui/material'
+import type { SelectChangeEvent } from '@mui/material/Select';
 
-import data from '../data/journey-events.json';
+
+import JourneyEvent from './JourneyEvent';
+import data from '../data/journey-data.json';
 
 const journeyEventsList = data.events;
-
-const getRandomEvent = () => {
-  const idx = Math.floor(Math.random() * journeyEventsList.length);
-  return journeyEventsList[idx];
-};
+const targets = data.roles;
+const event_types = data.event_types;
 
 const JourneyEventGenerator: React.FC = () => {
   const [currentEvent, setCurrentEvent] = useState({});
   const [log, setLog] = useState([journeyEventsList[0]]);
   const [eventCount, setEventCount] = useState(0)
+  const [region, setRegion] = useState("border");
+
+  const getRandomEvent = () => {
+    const target = getRandomTarget();
+    const event_type = getRandomEventType();
+    const targeted_events = getRoleEvents(target, event_type);
+    const idx = Math.floor(Math.random() * targeted_events.length);
+    return targeted_events[idx];
+  };
+
+  const getRoleEvents = (target: string, type: string) => {
+    return journeyEventsList.filter((event: object) => event.target == target && event.event == type )
+  }
+
+  const getRandomEventType = () => {
+    let event_type_idx1;
+    let event_type_idx2;
+    let event_type_idx;
+    let event_type;
+
+    switch (region) {
+      case "border": 
+        console.log('favoured event roll');
+        event_type_idx1 = Math.floor(Math.random() * event_types.length);
+        event_type_idx2 = Math.floor(Math.random() * event_types.length);
+        event_type_idx = Math.max(event_type_idx1, event_type_idx2)
+        event_type = event_types[event_type_idx];
+        return event_type;
+
+      case "wild":
+        console.log('straight event roll');
+        event_type_idx = Math.floor(Math.random() * event_types.length);
+        event_type = event_types[event_type_idx];
+        return event_type;
+
+      case "dark":
+        console.log('ill-favoured event roll');
+        event_type_idx1 = Math.floor(Math.random() * event_types.length);
+        event_type_idx2 = Math.floor(Math.random() * event_types.length);
+        event_type_idx = Math.min(event_type_idx1, event_type_idx2)
+        event_type = event_types[event_type_idx];
+        return event_type
+    }
+  }
+
+  const getRandomTarget = () => {
+    const target_idx = Math.floor(Math.random() * targets.length);
+    const target = targets[target_idx];
+    return target;
+  }
 
   const handleRandomize = () => {
     const event = getRandomEvent();
@@ -34,15 +83,30 @@ const JourneyEventGenerator: React.FC = () => {
     setLog([]);
   }
 
+  const handleRegionChange = (event: SelectChangeEvent) => {
+    setRegion(event.target.value);
+  }
+
   return (
     <div>
-      <div className="d-flex flex-row justify-content-center gap-1">
+      <div id="journey-form" className="d-flex flex-row justify-content-center align-items-center flex-grow-1 gap-1">
+        <span className="region-wrapper d-flex flex-row align-items-center gap-1">
+          <InputLabel id="region-label">Region</InputLabel>
+          <Select
+            labelId="region-label"
+            id="region"
+            value={region}
+            label="Region"
+            onChange={handleRegionChange}
+          >
+            <MenuItem value={"border"}>Border Land</MenuItem>
+            <MenuItem value={"wild"}>Wild Land</MenuItem>
+            <MenuItem value={"dark"}>Dark Land</MenuItem>
+          </Select>
+        </span>
         <Button variant="contained" onClick={handleRandomize} sx={{ mb: 2 }}>
           Next Event
         </Button>
-        <Button variant="contained" onClick={handleNewJourney} sx={{ mb: 2 }}>
-          New Journey
-        </Button>  
       </div>
       
       <JourneyEvent {...currentEvent} />
@@ -66,8 +130,16 @@ const JourneyEventGenerator: React.FC = () => {
           )}
         </AccordionDetails>
       </Accordion>
+
+      <div className="mt-4 d-flex flex-row justify-content-end gap-1">
+        <Button variant="contained" onClick={handleNewJourney} sx={{ mb: 2 }}>
+          New Journey
+        </Button>  
+      </div>
     </div>
   );
+
+  
 };
 
 export default JourneyEventGenerator;
